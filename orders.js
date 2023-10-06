f2b25a85f248681448097b0e5d4ac1e6c464491a
okolijerry <142677067+okolijerry@users.noreply.github.com>
2023-10-06 02:12:40 +0100

    jerryCodes

---
+++ orders.js
@@ -0,0 +1,33 @@
+const orders = [
+  {productName: 'Foldable Mini Drone',
+   productNumber: '34564',
+   paymentStatus:  'Due',
+   shipping: 'pending'
+
+},
+{productName: 'Foldable Mini Drone',
+productNumber: '34564',
+paymentStatus:  'Due',
+shipping: 'pending'
+
+},
+{productName: 'Foldable Mini Drone',
+productNumber: '34564',
+paymentStatus:  'paid',
+shipping: 'delivered'
+
+},
+{productName: 'Foldable Mini Drone',
+productNumber: '34564',
+paymentStatus:  'refunded',
+shipping: 'declined'
+
+},
+{productName: 'Foldable Mini Drone',
+productNumber: '34564',
+paymentStatus:  'paid',
+shipping: 'delivered'
+
+}
+
+]
